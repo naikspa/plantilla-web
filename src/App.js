c885b2c72723f008240d722bf13defa5c1f91c54
alenca <nicosavioparedes@gmail.com>
2023-06-21 22:57:26 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Carousel from './components/Carousel';
-import Products from './components/Products';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Carousel from "./components/Carousel";
+import Products from "./components/Products";
 
 function App() {
   return (
-  <>
-    <Navbar/>
-    <Carousel/>
-    <Products/>
+    <>
+      <Navbar />
+      <Carousel />
+      <div className="p-max-container">
+        <Products />
+      </div>
 
-    <Footer/>
-  </>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Products = () => {
-  let productos = [{name:"Airpods", price:237}, {name:"iPhone 14 Pro", price:1200}, {name:"iPod Touch", price:268}, {name:"Macbook", price:2460}]
-
+  let productos = [
+    { name: "Airpods", price: 237 },
+    { name: "iPhone 14 Pro", price: 1200 },
+    { name: "iPod Touch", price: 268 },
+    { name: "Macbook", price: 2460 },
+  ];
 
   return (
     <>
-    { 
-        productos.map( p => {return(
-            <div>
-                <h1 className='p-name'>{p.name}</h1>
-                <p className='p-price'>{p.price}</p>
-            </div>
-        )})
-    }
-    
+      {productos.map((p) => {
+        return (
+          <div className="p-container">
+            <h1 className="p-name">{p.name}</h1>
+            <p className="p-price">{p.price}</p>
+          </div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
